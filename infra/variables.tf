c2fa7e2b3af88cc14c95d715d87c73cb3a09a77d
Rik Ribbers <rik.ribbers@gmail.com>
2026-04-04 20:01:14 +0000

    updates

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,13 +6,14 @@ variable "app_version" {
 variable "app_image" {
   description = "The full path to the container image (without version)"
   type        = string
-} 
+}
 
 variable "gcp_project_name" {
   description = "The GCP project where to deploy the resources"
   type        = string
-} 
+}
 
 variable "gcp_default_region" {
   description = "The default GCP region where to deploy the resources"
+  type        = string
 }
