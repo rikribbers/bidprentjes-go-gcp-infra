d502e7d6ea97613c22b5f442ddae4f9b3c1d4565
Rik Ribbers <rik.ribbers@gmail.com>
2026-04-05 12:48:13 +0200

    Upgraded the versions (#13)
    * Upgraded the versions
    
    * updates

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,19 @@
 variable "app_version" {
   description = "The version tag for the bidprentjes-go container image"
   type        = string
-  default     = "v0.0.1"
 }
 
 variable "app_image" {
   description = "The full path to the container image (without version)"
   type        = string
-  default     = "docker.io/rikribbers/bidprentjes-go"
-} 
+}
+
+variable "gcp_project_name" {
+  description = "The GCP project where to deploy the resources"
+  type        = string
+}
+
+variable "gcp_default_region" {
+  description = "The default GCP region where to deploy the resources"
+  type        = string
+}
